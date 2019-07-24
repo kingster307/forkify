76a8b721898ee5e76df6f160744c99b65d28e065
Peter King <techking61@outlook.com>
2019-07-24 10:33:53 -0400

    stage 2 changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,45 @@
-// Global app controller
-import x from './test';
-const c = 22;
-console.log(`${x} ya knoww ${c}`);
+//controller 
+import Search from './models/Search';
+import * as searchView from "./views/searchView";
+import {elements} from "./views/base";
+
+// global state of the app
+const state = {};
+
+const controlSeach = async () => {
+    // get the query from the view
+    const query = searchView.getInput();
+
+    if (query){
+        //new search object && add it to state 
+        state.search = new Search(query);
+
+        //prepare UI for results 
+        
+
+        //search for recipieces ---> /returns promise 
+        await state.search.getResults();
+
+        //render results on UI
+        // console.log(state.search.result);
+    }
+
+};
+
+//event listener for the form 
+elements.searchForm.addEventListener("submit", e => {
+    //prevents the default action --> in this instance it wont reload the page 
+    e.preventDefault();
+    controlSeach();
+
+});
+
+
+
+// const search    = new Search("pizza");
+
+
+
+// console.log(search)
+// search.getResults();
+
